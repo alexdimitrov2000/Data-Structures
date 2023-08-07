7d163b047720e5a76ace16ef146227f94efa630e
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2023-08-07 17:39:04 +0300

    Created IAbstractQueue interface and implemented it by a Queue class.

---
+++ DataStructuresExercise/DataStructuresExercise/Contracts/IAbstractQueue.cs
@@ -0,0 +1,15 @@
+﻿namespace DataStructuresExercise.Contracts
+{
+    public interface IAbstractQueue<T> : IEnumerable<T>
+    {
+        int Count { get; }
+
+        bool Contains(T item);
+
+        void Enqueue(T item);
+
+        T Dequeue();
+
+        T Peek();
+    }
+}
---
+++ DataStructuresExercise/DataStructuresExercise/Queue.cs
@@ -0,0 +1,103 @@
+﻿namespace DataStructuresExercise
+{
+    using Contracts;
+    using System.Collections;
+
+    public class Queue<T> : IAbstractQueue<T>
+    {
+        private class Node
+        {
+            public T Value { get; set; }
+
+            public Node Next { get; set; }
+
+            public Node(T value) : this(value, null)
+            { }
+
+            public Node(T value, Node next)
+            {
+                Value = value;
+                Next = next;
+            }
+        }
+
+        private Node top;
+        private int count;
+
+        public int Count => this.count;
+
+        public bool Contains(T item)
+        {
+            Node current = this.top;
+
+            while (current != null)
+            {
+                if (current.Value!.Equals(item))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public void Enqueue(T item)
+        {
+            Node newNode = new Node(item);
+            this.count++;
+
+            if (this.top is null)
+            {
+                this.top = newNode;
+                return;
+            }
+
+            Node current = this.top;
+            while (current.Next is not null)
+            {
+                current = current.Next;
+            }
+
+            current.Next = newNode;
+        }
+
+        public T Dequeue()
+        {
+            if (this.top is null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            Node currentTop = this.top;
+            this.top = this.top.Next;
+            this.count--;
+
+            return currentTop.Value;
+        }
+
+        public T Peek()
+        {
+            if (this.top is null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.top.Value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = this.top;
+
+            while (current is not null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
