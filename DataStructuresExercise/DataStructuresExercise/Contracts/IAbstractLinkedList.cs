1f409a3934e86dc2d3c0c97b85fd55e8639f59a4
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2023-08-07 17:40:36 +0300

    Created IAbstractLinkedList interface.

---
+++ DataStructuresExercise/DataStructuresExercise/Contracts/IAbstractLinkedList.cs
@@ -0,0 +1,19 @@
+﻿namespace DataStructuresExercise.Contracts
+{
+    public interface IAbstractLinkedList<T> : IEnumerable<T>
+    {
+        int Count { get; }
+
+        void AddFirst(T item);
+
+        void AddLast(T item);
+
+        T GetFirst();
+
+        T GetLast();
+
+        T RemoveFirst();
+
+        T RemoveLast();
+    }
+}
